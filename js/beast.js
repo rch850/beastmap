32f4e846d9517a5097d142eed023f0ff643dc7ae
rch850 <rich850@gmail.com>
2014-01-25 11:40:27 +0900

    JavaScript整理

--- js/beast.js
+++ js/beast.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
+  // カルーセルのセットアップ
   $("#owl-example").owlCarousel();
-});
-(function() {
-  window.addEventListener("load", function() {
-    var map = new OpenLayers.Map("canvas");
-    var mapnik = new OpenLayers.Layer.OSM();
-    map.addLayer(mapnik);
 
-    //35.85455&lon=136.24695
-    var lonLat = new OpenLayers.LonLat(136.25, 35.85)
-      .transform(
-        new OpenLayers.Projection("EPSG:4326"),
-        new OpenLayers.Projection("EPSG:900913")
-      );
-    map.setCenter(lonLat, 9);
-  });
-})();
+  // マップの表示
+  var map = new OpenLayers.Map("canvas");
+  var mapnik = new OpenLayers.Layer.OSM();
+  map.addLayer(mapnik);
+
+  //35.85455&lon=136.24695
+  var lonLat = new OpenLayers.LonLat(136.25, 35.85)
+    .transform(
+      new OpenLayers.Projection("EPSG:4326"),
+      new OpenLayers.Projection("EPSG:900913")
+    );
+  map.setCenter(lonLat, 9);
+});
